bfbf2903e28c68adf2a369b1b57f5d942573c3ff
GelynRabe <100335096+GelynRabs@users.noreply.github.com>
2022-11-14 16:07:08 +0800

    registration2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import LandingPage from './components/LandingPage';
 import Register from './components/Register';
+import Registration from './components/Registration';
+
 
 function App() {
   return (
     <div className='App'>
       <div className='App-container'>
         <NavBar/>
-        <Register/>
+        {/* <Register/> */}
+       <Registration/>
+       
       </div>
     </div>
   );
