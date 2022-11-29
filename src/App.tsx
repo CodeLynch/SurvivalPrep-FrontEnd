a157ff0a92179efa0c491fd251e4b8447bf2fac0
Vergil <88496530+CodeLynch@users.noreply.github.com>
2022-11-29 11:51:39 +0800

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,17 +18,17 @@ export default function App() {
     return (
       <div className='App'>
         <div className='App-container'>
-          <NavBar/>
-          {/* { <Router>
+          { <Router>
               <Routes>
                 <Route path="/" element={<div><NavBar/><LandingPage/></div>}></Route>
                 <Route path="/register" element={<div><NavBar/><Registration/></div>}></Route>
                 <Route path="/forums" element={<div><NavBar/><ForumsPage/></div>}></Route>
                 <Route path="/family" element={<div><NavBar/><FamilyPage/></div>}></Route>
                 <Route path="/tips" element={<div><NavBar/><TipsPage/></div>}></Route>
+                <Route path="/profile" element={<div><NavBar/><Profilepage/></div>}></Route>
               </Routes>
-          </Router> } */}
-          <Profilepage/>
+          </Router> }
+         
         </div>
       </div>
     );
