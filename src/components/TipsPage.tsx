51dcab078337f448c203e53a0229ea970d6b01df
Vergil <88496530+CodeLynch@users.noreply.github.com>
2022-11-29 10:55:09 +0800

    modified TipsPage

--- src/components/TipsPage.tsx
+++ src/components/TipsPage.tsx
@@ -22,8 +22,7 @@ export default function TipsPage() {
 
   useEffect(()=>{
     TipsService.getAllTips().then((response)=>{
-    console.log(response.data)
-    // setTipsList(response.data)
+    setTipsList(response.data)
   });},[])
 
   return (
